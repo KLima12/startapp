d3e166b18b1a87b9908d39eb79971a6025041816
Kauã Lima <kaualimaa1233@gmail.com>
2025-09-05 11:59:11 -0300

    Avançando nos estudos

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,28 @@
-import { View, Text} from "react-native"
+import { View, Text, StyleSheet, Alert, Button} from "react-native"
 export default function Index(){ 
+    function handleMessage() { 
+        return Alert.alert("Olá, Kauã!")
+    }
     return ( 
-        <View>
-            <Text>Hello World!</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Hello, World!</Text>
+
+            <Button title="Enviar" onPress={handleMessage} />
         </View>
         
     )
 }
+
+
+const styles = StyleSheet.create({ 
+    container: { 
+        flex: 1,
+        justifyContent: 'center', 
+        alignItems: 'center',
+    },
+    title: { 
+        color: "red", 
+        fontSize: 24,
+        fontWeight: "bold",
+    }
+})
